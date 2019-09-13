2c868855ac8f4d7c41fdfbe325d75d51a1cb4969
stevenvandyke <37986733+stevenvandyke@users.noreply.github.com>
2019-09-12 21:00:41 -0500

    Adding the controllers

---
+++ Controllers/RatingsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class RatingsController : ApiController
+    {
+        // POST api/values
+        public void Post(Rating rating)
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                var existingRestaurant = entitites.Restaurants.FirstOrDefault(r => r.Id == rating.RestaurantId);
+
+                if (existingRestaurant != null)
+                {
+                    var previousAverageRating = existingRestaurant.AverageRating;
+
+                    existingRestaurant.NumberOfRatings += 1;
+                    existingRestaurant.TotalRatings += rating.Value;
+                    existingRestaurant.AverageRating = existingRestaurant.TotalRatings / (decimal)existingRestaurant.NumberOfRatings;
+                    entitites.SaveChanges();
+
+                    if (previousAverageRating >= 2 && existingRestaurant.AverageRating < 2)
+                    {
+                        // Trigger an event notification when the average rating transitions below 2
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class RestaurantsController : ApiController
+    {
+        // GET: api/Restaurants
+        public IEnumerable<Restaurant> Get()
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                return entitites.Restaurants.ToList();
+            }
+        }
+
+        // GET: api/Restaurants/5
+        public Restaurant Get(int restaurantId)
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                return entitites.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+            }
+        }
+
+        // POST: api/Restaurants
+        public void Post(Restaurant restaurant)
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                entitites.Restaurants.Add(restaurant);
+                entitites.SaveChanges();
+            }
+        }
+
+        // PUT: api/Restaurants/5
+        public void Put(int restaurantId, Restaurant restaurant)
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                var existingRestaurant = entitites.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+
+                if (existingRestaurant != null)
+                {
+                    existingRestaurant.Name = restaurant.Name;
+                    existingRestaurant.Address = restaurant.Address;
+                    existingRestaurant.Description = restaurant.Description;
+                    existingRestaurant.Hours = restaurant.Hours;
+                    existingRestaurant.NumberOfRatings = restaurant.NumberOfRatings;
+                    existingRestaurant.TotalRatings = restaurant.TotalRatings;
+                    existingRestaurant.AverageRating = restaurant.AverageRating;
+                    entitites.SaveChanges();
+                }
+            }
+        }
+
+        // DELETE: api/Restaurants/5
+        public void Delete(int restaurantId)
+        {
+            using (var entitites = new RestaurantDbEntities())
+            {
+                var existingRestaurant = entitites.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+
+                if (existingRestaurant != null)
+                {
+                    entitites.Restaurants.Remove(existingRestaurant);
+                    entitites.SaveChanges();
+                }
+            }
+        }
+    }
+}
